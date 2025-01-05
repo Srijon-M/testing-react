58e319650c9e120d9a796fed82baaa4c330719f2
Srijon-M <connectwithsriji@gmail.com>
2025-01-05 13:17:07 +0530

    getBy.. query

--- src/components/JobLocation/JobLocation.test.tsx
+++ src/components/JobLocation/JobLocation.test.tsx
@@ -5,12 +5,59 @@ import React from "react";
 describe("JobLocation", () => {
     test("component renders correctly", () => {
         render(<JobLocation />);
-        const textElement = screen.getByRole("textbox");
+
+        const pageHeading = screen.getByRole("heading", {
+            level: 1
+        });
+        expect(pageHeading).toBeInTheDocument();
+
+        const pageSubHeading = screen.getByRole("heading", {
+            level: 2
+        });
+        expect(pageSubHeading).toBeInTheDocument();
+
+        const paragraphElement = screen.getByText("All fields are mandetory");
+        expect(paragraphElement).toBeInTheDocument();
+
+        const textElement = screen.getByRole("textbox", {
+            name: "Name"
+        });
         expect(textElement).toBeInTheDocument();
+
+        const nameElement = screen.getByLabelText("Name", {
+            selector: "input"
+        });
+        expect(nameElement).toBeInTheDocument();
+
+        const nameElement2 = screen.getByPlaceholderText("Enetr your name");
+        expect(nameElement2).toBeInTheDocument();
+
+        const nameElement3 = screen.getByDisplayValue("Srijon");
+        expect(nameElement3).toBeInTheDocument();
+
+        const imageElement = screen.getByAltText("A person with a laptop");
+        expect(imageElement).toBeInTheDocument();
+
+        const closeElement = screen.getByTitle("close");
+        expect(closeElement).toBeInTheDocument();
+
+        const customElement = screen.getByTestId("custom-element");
+        expect(customElement).toBeInTheDocument();
+
+        const textAreaElement = screen.getByRole("textbox", {
+            name: "Bio"
+        });
+        expect(textAreaElement).toBeInTheDocument();
+
         const jobLocationElement = screen.getByRole("combobox");
         expect(jobLocationElement).toBeInTheDocument();
+        
         const termsElement = screen.getByRole("checkbox");
         expect(termsElement).toBeInTheDocument();
+
+        const termsElement2 = screen.getByLabelText("I agree to the terms and conditions");
+        expect(termsElement2).toBeInTheDocument();
+        
         const submitBtnElement = screen.getByRole("button");
         expect(submitBtnElement).toBeInTheDocument();
     })
--- src/components/JobLocation/JobLocation.tsx
+++ src/components/JobLocation/JobLocation.tsx
@@ -2,29 +2,47 @@ import React from 'react';
 
 const JobLocation = () => {
     return(
-        <form>
-            <div>
-                <label htmlFor="name">Name</label>
-                <input type="text" id="name" />
-            </div>
-            <div>
-                <label htmlFor="job-location">Job location</label>
-                <select id="job-location">
-                    <option value="">Select a country</option>
-                    <option value="US">United States</option>
-                    <option value="GB">United Kingdom</option>
-                    <option value="CA">Canada</option>
-                    <option value="IN">India</option>
-                    <option value="AU">Australia</option>
-                </select>
-            </div>
-            <div>
-                <label>
-                    <input type="checkbox" id="terms" /> I agree to the terms and conditions
-                </label>
-            </div>
-            <button>Submit</button>
-        </form>
+        <>
+            <h1>Job Application Form</h1>
+            <h2>Section 1</h2>
+            <p>All fields are mandetory</p>
+            <img src="https://via.placeholder.com/150" alt="A person with a laptop"/>
+            <span title="close">X</span>
+            <div data-testid="custom-element">Custom HTML Element</div>
+            <form>
+                <div>
+                    <label htmlFor="name">Name</label>
+                    <input 
+                        type="text" 
+                        id="name" 
+                        placeholder='Enetr your name' 
+                        value="Srijon"    
+                        readOnly
+                    />
+                </div>
+                <div>
+                    <label htmlFor='bio'>Bio</label>
+                    <textarea name="bio" id="bio"></textarea>
+                </div>
+                <div>
+                    <label htmlFor="job-location">Name</label>
+                    <select id="job-location">
+                        <option value="">Select a country</option>
+                        <option value="US">United States</option>
+                        <option value="GB">United Kingdom</option>
+                        <option value="CA">Canada</option>
+                        <option value="IN">India</option>
+                        <option value="AU">Australia</option>
+                    </select>
+                </div>
+                <div>
+                    <label>
+                        <input type="checkbox" id="terms" /> I agree to the terms and conditions
+                    </label>
+                </div>
+                <button>Submit</button>
+            </form>
+        </>
     )
 }
 
